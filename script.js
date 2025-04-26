a32ea9e9498d3ea9eaf1bc8559a9d45807161949
Fernando Souza <fernandofvsouza@icloud.com>
2025-04-26 12:26:53 +0100

    Fim

---
+++ script.js
@@ -0,0 +1,15 @@
+function acender(){
+    var lampadaApagada = document.getElementById('img')
+    lampadaApagada.src ='assets/lampadaligada.avif'
+}
+
+function apagar(){
+    var lampadaAcesa = document.getElementById('img')
+    lampadaAcesa.src = 'assets/lampada.avif'
+}
+
+function quebrar(){
+    var lampadaQuebrada = document.getElementById('img')
+    lampadaQuebrada.src = 'assets/lampadaquebrada.jpg'
+}
+
